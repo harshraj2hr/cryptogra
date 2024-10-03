5502f1978245fcb0189c2bab3cec15072fad510f
harsh <harshraj2hr1408@gmail.com>
2024-10-03 07:54:12 +0530

    route

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainComponent from './main-component/MainComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Router  basename="/cryptogra">
+      <Router basename="/cryptogra">
         <Routes>
           <Route path="/cryptograd/document-analyser" element={<MainComponent />} />
-          {/* for the time being*/}
           <Route path="/" element={<MainComponent />} /> 
         </Routes>
       </Router>
     </div>
   );
 }
-
 export default App;
