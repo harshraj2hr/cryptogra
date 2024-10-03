e42dafbbcc2de573ef01ce239d4283d8ea5c93ca
harsh <harshraj2hr1408@gmail.com>
2024-10-03 07:41:37 +0530

    route

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router  basename="/cryptogra">
         <Routes>
           <Route path="/cryptograd/document-analyser" element={<MainComponent />} />
           {/* for the time being*/}
